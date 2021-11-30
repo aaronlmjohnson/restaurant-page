92204c7e38b2c63e9b504984ffb8a71c22c9f32b
Aaron <ajonaaronajon@yahoo.com>
2021-11-29 23:10:32 -0600

    basic nav menu structure made

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import './style.css';
+import { DomHelper } from './DomHelper'
+
+(()=>{
+    const content = document.getElementById("content");
+
+    const createNavBar = ()=>{
+        const nav = document.createElement("nav");
+        const navItems = document.createElement("ul");
+
+        nav.appendChild(navItems);
+        createNavListItems(navItems);
+        content.appendChild(nav);
+    };
+    
+    const createNavListItems = (navItems)=>{
+        ["Menu", "About", "Contact"].forEach((item)=>{
+            const navItem = document.createElement("li");
+            console.log(item);
+            navItem.innerText = item;
+            navItems.appendChild(navItem);
+        });
+    };
+    
+    createNavBar();
+})();
+
+
 
