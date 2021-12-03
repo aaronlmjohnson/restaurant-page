de5e36c39f4ce5a69b17ad7c2ea25a2eb91c4fc7
Aaron <ajonaaronajon@yahoo.com>
2021-12-03 09:58:58 -0600

    started on food-section of homepage

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ export const Home = (()=>{
 
     const _createVideo = ()=>{
         const video = document.createElement("video");
-        video.autoplay = true;
+        //video.autoplay = true;
         video.loop = true;
         video.muted = true;
         video.id = "cover-video"
@@ -26,5 +26,18 @@ export const Home = (()=>{
         return video;
     };
 
+    const _createFoodSection = ()=>{
+        const section = document.createElement("div");
+        section.id = "food-section";
+        const topSection = document.createElement("div");
+        topSection.classList.add("top-section");
+        const header = document.createElement("h1");
+        header.innerText = "Beer, wings, and all you can eat potato salad only at Crunchy's"
+        topSection.appendChild(header)
+        section.appendChild(topSection);
+        content.appendChild(section);
+    };
+
     _createVideoContainer();
+    _createFoodSection();
 })();
